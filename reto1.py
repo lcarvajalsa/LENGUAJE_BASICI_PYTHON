a3055ee823896a365fc7408e8cb5a03c0496c389
lcarvajalsa <110641945+lcarvajalsa@users.noreply.github.com>
2023-05-19 20:27:54 -0500

    completado reto uno

--- reto1.py
+++ reto1.py
@@ -7,7 +7,7 @@ opera=int(input("Selecciona UNO para abrir la ficha \n"))
 while opera==1:
     elem1=int(input("Deseas agrege un instructor a la ficha UNO para si, DOS para no\n"))
     if elem1==1:
-#/ Permita agregar el nombre de un instructor a la lista
+    #/Permita agregar el nombre de un instructor a la lista
         elem=str(input("Agrege Nombre del Instructor\n"))
         ficha.append(elem)
         print(ficha)
@@ -23,7 +23,7 @@ while opera==1:
     else:
         print("No cumple con las caracteristicas de Ingreso")
         break   
-#/Listar los instructores que están en la lista
+    #/Listar los instructores que están en la lista
     elem2=int(input("Deseas ver la listar de instructores asignados  de forma A-Z UNO para si DOS para no\n")) 
     if elem2==1:
         ficha.sort(reverse=False)
@@ -35,18 +35,18 @@ while opera==1:
             print(i,e)
     else:
         print("Confiemado los registros actuales ",ficha)
-#/Modificar un elemento de la lista (seleccionado por el usuario)
+    #/Modificar un elemento de la lista (seleccionado por el usuario)
     dato=int(input("Desea modificar un Instructor de la ficha UNO para si, DOS para no\n"))
     if dato==1:
         dato1=int(input("Selecciona numero del Instructor a modificar\n"))
 
         dato2=str(input("Ingrese nombre\n"))
         ficha[dato1]=dato2
-       
+        
         print("Confirmacion de ficha a la fecha ",ficha)
     else:
         print("Comprobando instructores a la fecha ",ficha)
-#/Borrar un elemento de la lista (seleccionado por el usuario)
+    #/Borrar un elemento de la lista (seleccionado por el usuario)
     dato=int(input("Desea retirar un Instructor de la ficha UNO para si, DOS para no\n"))
     if dato==1:
         dato1=str(input("Selecciona el Instructor\n"))
@@ -57,8 +57,8 @@ while opera==1:
     opera1=int(input("Desea cerrar la ficha UNO para si y DOS para no\n"))
     if opera1==1:
         opera=2
-#/Buscar un elemento particular de la lista por su nombre sin importar las mayúsculas o minúsculas.
+    #/Buscar un elemento particular de la lista por su nombre sin importar las mayúsculas o minúsculas.
 
-#/Ordenar la lista de instructores de la A-Z
-    
-print("Cerraste la ficha ")
+    #/Ordenar la lista de instructores de la A-Z
+
+    print("Cerraste la ficha ")
