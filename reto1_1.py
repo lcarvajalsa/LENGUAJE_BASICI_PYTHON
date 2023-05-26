e9a82d0441a0ebcc3ec574f2ba7dc5b12027a44f
lcarvajalsa <110641945+lcarvajalsa@users.noreply.github.com>
2023-05-26 10:52:09 -0500

    Terminado

--- reto1_1.py
+++ reto1_1.py
@@ -2,10 +2,12 @@ lista=[]
 
 
 nom=str(input("Agregar el nombre del instructor \n"))
+nom=nom.lower()
 lista.append(nom)
 aceptar=int(input("Desea agregar un instructor mas a la lista UNO para si, DOS para NO \n"))
 while aceptar==1:
     nom=str(input("Agregar nuevo instructor \n"))
+    nom=nom.lower()
     lista.append(nom)
     aceptar=int(input("Desea agregar otro instructor mas a la lista UNO para si, DOS para NO \n"))
     for i, e in enumerate(lista):
@@ -14,6 +16,7 @@ dato=int(input("Desea modificar el nombre del Instructor UNO para si, DOS para n
 while dato==1:
         dato1=int(input("Selecciona numero del Instructor a modificar\n"))
         nom=str(input("Ingrese nombre\n"))
+        nom=nom.lower()
         lista[dato1]=nom
         dato=int(input("Desea modificar el nombre de otro Instructor UNO para si, DOS para no\n"))
         for i, e in enumerate(lista):
@@ -21,6 +24,7 @@ while dato==1:
 modificar=int(input("Desea eliminar un Instructor de la lista UNO para si, DOS para no\n"))
 while modificar==1:
     nom=str(input("Seleccionar el nombre del Instructor\n"))
+    nom=nom.lower()
     lista.remove(nom)
     modificar=int(input("Desea eliminar otro Instructor de la lista UNO para si, DOS para no\n"))
     for i, e in enumerate(lista):
@@ -28,8 +32,8 @@ while modificar==1:
 buscar=int(input("Desea buscar un Instructor de la lista UNO para si, DOS para no\n"))
 while buscar==1:
     nom=str(input("Nombre del Instructor\n"))
-    nom_lower = nom.lower()
-    esta_lista=lista.count(nom_lower)
+    nom = nom.lower()
+    esta_lista=lista.count(nom)
     if esta_lista>0:
         print ("esta en la lista")
         buscar=int(input("Desea buscar otro Instructor de la lista UNO para si, DOS para no\n"))
